fe2c61c5f0a113284fa3e963bc56438dc308f43c
Ben-1819 <ben.brown20332@gmail.com>
2025-04-22 16:15:22 +0100

    feat: Imported all necessary components
    Import all components that will need to be used in main.js

--- src/main.js
+++ src/main.js
@@ -2,5 +2,12 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import Player from './components/Player.vue'
+import Dealer from './components/Dealer.vue'
+import Results from './components/Results.vue'
 
-createApp(App).mount('#app')
+createApp(App)
+.component("Player", Player)
+.component("Dealer", Dealer)
+.component("Results", Results)
+.mount('#app')

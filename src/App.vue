c64ea4508762b8437fc75a516512d41af055c83d
Ben-1819 <ben.brown20332@gmail.com>
2025-04-22 16:56:28 +0100

    feat: Created variables, mounted hook and setupDeck method
    Created the suits array which holds the card suits, the values array
    which holds card values, the deck array which starts empty but is
    created by looping through the suits and values arrays in the setupDeck
    method. Created the mounted hook which runs the setupDeck method when
    App.vue is mounted.

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,57 @@
   </main>
 </template>
 
+<script>
+export default{
+  data() {
+    return {
+      // Create the suits array
+      suits: ["Hearts", "Spades", "Clubs", "Diamonds"],
+      // Create the values array
+      values: ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"],
+      // Create the deck array
+      deck: [],
+      // Create the playerTurn variable and set it to false
+      playerTurn: false,
+      // Create the dealerTurn variable and set it to false
+      dealerTurn: false,
+    }
+  },
+  mounted () {
+    // Call the setupDeck method when the component is mounted
+    this.setupDeck();
+    console.log(this.deck);
+  },
+  methods: {
+    setupDeck() {
+      // Loop through the suits array
+      this.suits.forEach((suit) => {
+        // Loop through the values array
+        this.values.forEach((value) => {
+          // Create the card variable
+          const card = ""+value+" of "+suit+""
+          // Push the card variable to the deck
+          this.deck.push(card);
+        });
+      });
+      //Use the Fisher-Yates algorithim to shuffle the deck
+      // Create variables for the index value, the random number and the temporary value
+      let i = this.deck.length, j, temp;
+      while(--i > 0){
+        // Create a random number and store it in a variable
+        j = Math.floor(Math.random() * (i + 1));
+        // Create a temporary position for the item of the random number
+        temp = this.deck[j];
+        // Swap the temp item with the position of the last item in the array
+        this.deck[j] = this.deck[i];
+        // Swap the last item with the position of the temp item
+        this.deck[i] = temp;
+      }
+    }
+  },
+}
+</script>
+
 <style scoped>
 header {
   line-height: 1.5;

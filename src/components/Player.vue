78dce6c1d737fbd5001691bd9bc160e069f5683e
Ben-1819 <ben.brown20332@gmail.com>
2025-04-22 17:12:30 +0100

    feat: Created the data variables for the player component
    Created the playerCards, playerScore and playerAces variables.

---
+++ src/components/Player.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                // Create playerCards array, it is empty at the start
+                playerCards: [],
+                // Create the playerScore variable, it begins at 0
+                playerScore: 0,
+                // Create the playerAces variable, it begins at 0
+                playerAces: 0,
+            }
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>

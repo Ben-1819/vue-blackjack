afefb6fb452269f98328a899c9f3192d1d44c9da
Ben-1819 <ben.brown20332@gmail.com>
2025-04-23 09:48:35 +0100

    feat: Defined the props and data for Results.vue
    Defined the playerScore, playerCards, dealerScore and dealerCards props
    and created the winner variable.

---
+++ src/components/Results.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <h1>Results</h1>
+        <p>Player Score: {{ playerScore }}</p>
+        <p>Dealer Score: {{ dealerScore }}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                // Create the winner variable and set it to null
+                winner: null,
+            }
+        },
+        props: {
+            // playerScore prop, contains the playerScore variable
+            playerScore: {
+                type: Number,
+            },
+            // dealerScore prop, contains the dealerScore variable
+            dealerScore: {
+                type: Number,
+            },
+            // playerCards prop, contains the playerCards variable
+            playerCards:{
+                type: Array,
+            },
+            // dealerCards prop, contains the dealerCards variable
+            dealerCards:{
+                type: Array,
+            },
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>

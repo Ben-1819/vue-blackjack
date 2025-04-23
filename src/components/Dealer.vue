c3000a3617b868ba060a3080ba6477ad0e1c0770
Ben-1819 <ben.brown20332@gmail.com>
2025-04-23 08:32:48 +0100

    feat: Created the calculateScore method for Dealer.vue
    Created the calculateScore method in Dealer.vue, this method will loop
    through the dealers cards and split the rank, then it runs a switch
    statement on the split rank to decide how much to increase the dealers
    score by.

--- src/components/Dealer.vue
+++ src/components/Dealer.vue
@@ -47,7 +47,46 @@
                 this.calculateScore();
             },
             calculateScore(){
-                
+                // Set the dealers score to 0
+                this.dealerScore = 0;
+                // Set dealerAces to 0
+                this.dealerAces = 0;
+                // loop through each index of the dealerCards array
+                this.dealerCards.forEach((card)=>{
+                    // Use split to get the rank of the card
+                    const rank = card.split(" ")[0]
+                    console.log(rank);
+                    // Check the rank of the card
+                    switch(card){
+                        // If the card is an ace
+                        case "A":
+                            // Add 11 to dealerScore
+                            this.dealerScore += 11;
+                            // Increase dealerAces
+                            this.dealerAces++;
+                            // Break out of the switch statement
+                            break;
+                        // If the card is a king
+                        case "K":
+                            // Fallthrough to if the card is a queen
+                        // If the card is a queen
+                        case "Q":
+                            // Fallthrough to if the card is a jack
+                        // If the card is a jack
+                        case "J":
+                            // Add 10 to dealerScore
+                            this.dealerScore += 10;
+                            // Break out of the switch statement
+                            break;
+                        // If the card is a normal card
+                        default:
+                            // Use Number() to convert the rank of the card to an integer
+                            // And add it to dealerScore
+                            this.dealerScore += Number(rank);
+                    }
+                });
+                // Call the aces method
+                this.aces()
             },
             aces(){
 
